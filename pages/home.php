84233dce281b536af9a0b4b11d1d2c59d4723a73
élève 7 <lepaftordu@proton.me>
2026-03-03 11:00:08 +0100

    tony le plus beau

---
+++ pages/home.php
@@ -0,0 +1,9 @@
+<section>
+    <h2>Bienvenue sur la page Home</h2>
+    <p>
+        Ceci est la page d'accueil du site Mappix. Ici, vous pouvez découvrir nos contenus et naviguer facilement.
+    </p>
+    <p>
+        Cette page est conçue pour être simple et fonctionnelle tout en montrant la structure PHP réutilisable.
+    </p>
+</section>
